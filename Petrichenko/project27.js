3bfa30d72ae4e3b0da6f977b435e9caa6e082101
gavrilovdmitriy <gavrilovdmitriy@i.ua>
2025-08-29 01:51:04 +0300

    длт

---
+++ Petrichenko/project27.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const str = "test";
+
+const arr = [1, 2, 4];
+
+console.log(arr.length);
---
+++ script2.js
@@ -0,0 +1,23 @@
+// let number = 5;
+
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// leftBorderWidth = 2;
+// console.log(leftBorderWidth);
+
+// let result = confirm("are you here?");
+// console.lot(result);
+
+// const answer = prompt("are you 18?", "18");
+// console.log(answer + 7);
+
+const answers = [];
+
+answers[0] = prompt("what is your name,", "");
+answers[1] = prompt("what is your second name,", "");
+answers[2] = prompt("how old are you,", "");
+
+document.write(answers);
